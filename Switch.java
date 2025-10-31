b1519a0fee8ad3a3139767a939c62ec77f69f771
Hrishikeshchauhan09 <17hrishikeshchauhan@gmail.com>
2025-10-31 18:54:26 +0530

    new

--- Switch.java
+++ Switch.java
@@ -21,6 +21,42 @@ public class Switch {
      }
 
      System.out.println("\n----------------------------");
+    
+
+    
+       // Switch statement for days  (no user input)
+       int day = 3; // Change this value to test differnt days
+       String dayName;
+
+       switch (day) {
+        case 0:
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wedneday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+            dayName = "Invalid day";
+            break;
+            }
+
+      System.out.println("Today is " + dayName);
   }
+
   
 }
