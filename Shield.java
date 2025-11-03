e754097a2db8b32023ad3aa72da3a3446af1946f
Hrishikeshchauhan09 <17hrishikeshchauhan@gmail.com>
2025-11-03 19:05:53 +0530

    new

---
+++ Shield.java
@@ -0,0 +1,17 @@
+class Avengers{
+
+  void mission() {
+     System.out.println("Mission started....");
+  }
+}
+
+
+public class Shield {
+
+  public static void main(String[] args) {
+    
+    Avengers a = new Avengers();
+    a.mission();
+  }
+  
+}
