602f3aff350719abd857451529df661a969a19f4
Hrishikeshchauhan09 <17hrishikeshchauhan@gmail.com>
2025-11-14 17:12:43 +0530

    new

---
+++ Inherit.java
@@ -0,0 +1,32 @@
+class Parent {
+
+  void parentProperty(){
+    System.out.println("parent property");
+  }
+
+}
+
+class Child extends Parent {
+
+  void ChildProperty() {
+    System.out.println("Child Property");
+  }
+
+}
+
+class GrandChild extends Child {
+
+  void childProperty() {
+    System.out.println("GrandChild Property");
+  }
+
+}
+
+public class Inherit {
+
+  public static void main(String[] args) {
+    GrandChild GC = new GrandChild();
+    GC.Childproperty();
+  }
+  
+}
