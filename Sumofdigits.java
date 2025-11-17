99c0ae4591d6960440cc4ff74f7de6efb0bb1be2
Hrishikeshchauhan09 <17hrishikeshchauhan@gmail.com>
2025-11-17 18:43:26 +0530

    new

--- Sumofdigits.java
+++ Sumofdigits.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Sumofdigits {
   public static void main(String[] args){
+    Scanner scanner = new
     Scanner(System.in);
 
     System.out.println("Enter the number:");
