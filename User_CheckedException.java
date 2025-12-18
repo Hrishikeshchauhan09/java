4dc3ff2e5bfef4a120a4f785700ecec1254dcf5a
Hrishikeshchauhan09 <17hrishikeshchauhan@gmail.com>
2025-12-18 19:03:55 +0530

    new

---
+++ User_CheckedException.java
@@ -0,0 +1,20 @@
+import javax.naming.InsufficientResourcesException;
+
+class GaribException extends Exception {
+    public GaribException(String message) {
+        super(message);
+    }
+}
+
+class User_CheckedException {
+    double balance = 5000;
+
+    void withdraw(double amount) throws GaribException{
+        if (amount > balance) {
+            throw new GaribException("Low balance in account");
+        }
+balance -= amount;
+System.out.println("Withdrawal Successful. ");
+    }
+    
+}
